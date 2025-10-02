e96d537239339e5782af8edd7293ee4fa3959174
glowedjellys <235586137+glowedjellys@users.noreply.github.com>
2025-10-02 01:28:57 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/page.tsx
@@ -0,0 +1,80 @@
+import styles from '../styles/Home.module.scss';
+import Link from 'next/link';
+import {
+  CommentIcon,
+  IssueIcon,
+  StarIcon,
+  ForkIcon,
+  GitHubIcon
+} from './icons';
+import { fetchIssueAndRepoData } from '../lib/github';
+import Explanation from './explanation';
+import { Time } from './time-ago';
+
+export default async function Page() {
+  const { issues, forks_count, stargazers_count } =
+    await fetchIssueAndRepoData();
+
+  return (
+    <main className={styles.main}>
+      <Explanation />
+      <div className={styles.repo}>
+        <div className={styles.repo_title}>
+          <GitHubIcon />{' '}
+          <a
+            href="https://github.com/vercel/on-demand-isr"
+            target="_blank"
+            rel="noreferrer"
+          >
+            vercel
+          </a>{' '}
+          / <Link href="/">on-demand-isr</Link>
+        </div>
+        <div className={styles.forks_stars}>
+          <a
+            href="https://github.com/vercel/on-demand-isr/fork"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <ForkIcon /> {new Number(forks_count).toLocaleString()}
+          </a>
+          <a
+            href="https://github.com/vercel/on-demand-isr"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <StarIcon /> {new Number(stargazers_count).toLocaleString()}
+          </a>
+        </div>
+      </div>
+      <div className={styles.issues}>
+        {issues.map((issue: any) => (
+          <IssueLink key={issue.number} issue={issue} />
+        ))}
+      </div>
+    </main>
+  );
+}
+
+function IssueLink({ issue }: { issue: any }) {
+  return (
+    <Link href={`/${issue.number}`} className={styles.issue}>
+      <div className={styles.left}>
+        <IssueIcon />
+        <div>
+          <div className={styles.issue_title}>{issue.title}</div>
+          <div className={styles.issue_desc}>
+            {`#${issue.number} opened `}
+            {` by ${issue.user.login}`}
+          </div>
+        </div>
+      </div>
+      <div className={styles.right}>
+        <Time time={issue.created_at} />
+        <div className={styles.comment_count}>
+          <CommentIcon /> {new Number(issue.comments).toLocaleString()}
+        </div>
+      </div>
+    </Link>
+  );
+}
